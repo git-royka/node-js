5117475df549151ce4be63ff9fb32d377690725b
git-royka <46698567+git-royka@users.noreply.github.com>
2019-10-23 18:33:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 var file_readed = fs.readFileSync('counter.txt');
 var visits = file_readed;
+var dockerid = process.env.DOCKERID;
 var server = http.createServer(function(req, res){
 
 if (req.url === '/favicon.ico') { }
@@ -19,7 +20,7 @@ else {visits++};
   process.stdout.write = write(process.stdout.write);
   res.writeHead(200,{'Content-Type': 'text/plain; charset=utf-8'});
   fs.writeFileSync('counter.txt', visits);
-  res.end('Сайт посетили '+visits+' пользователя V3.3');
+  res.end('Сайт посетили '+visits+' пользователя V3.3 '+dockerid+'');
 });
 var port = process.env.PORT || 3434;
 var ip = process.env.IP;
