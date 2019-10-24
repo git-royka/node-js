532f30cb405fd3f943e70481ded8aa2c37a5af07
Maksym <maksym@adminpdfs-MacBook-Pro.local>
2019-10-24 11:08:54 +0300

    + random app№

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var express = require('express');
 var app = express();
 var file_readed = fs.readFileSync('counter.txt');
 var visits = file_readed;
-var dockerid = process.env.DOCKERID;
+//var dockerid = process.env.DOCKERID;
+var random = Math.random();
 var server = http.createServer(function(req, res){
 
 if (req.url === '/favicon.ico') { }
@@ -20,7 +21,7 @@ else {visits++};
   process.stdout.write = write(process.stdout.write);
   res.writeHead(200,{'Content-Type': 'text/plain; charset=utf-8'});
   fs.writeFileSync('counter.txt', visits);
-  res.end('Сайт посетили '+visits+' пользователя V3.3 '+dockerid+'');
+  res.end('Сайт посетили '+visits+' пользователя V3.3 APP№'+random+'');
 });
 var port = process.env.PORT || 3434;
 var ip = process.env.IP;
